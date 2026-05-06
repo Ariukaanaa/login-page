a44916025c71c8e1bb621f83c67b0a7a18597929
Ariuka <ariunbayar443@gmail.com>
2026-05-06 07:06:24 +0200

    aa

--- login/index.js
+++ login/index.js
@@ -7,8 +7,6 @@ const devLoginn = document.getElementById("login");
 // ariuka shuu odoo ene deer conflit garhgui bol shantarlaa
 
 
-
-
 registerBtn.addEventListener('click', (e) => {
     e.preventDefault();
     container.classList.add("active");
@@ -18,5 +16,7 @@ loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
     container.classList.remove("active");
 });
+
+// ene shine PR hiiilt
 
 
